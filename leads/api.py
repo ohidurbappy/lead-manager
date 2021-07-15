fcd38355918379bee0b49d2e3c4650f67bc3af10
Ohidur Rahman Bappy <ohidurbappy@gmail.com>
2021-07-15 18:42:25 +0600

    first-section

---
+++ leads/api.py
@@ -0,0 +1,14 @@
+from leads.models import Lead
+from rest_framework import permissions,viewsets
+from .serializers import LeadSerializer
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset=Lead.objects.all()
+    permission_classes=[
+        permissions.AllowAny
+    ]
+
+    serializer_class=LeadSerializer
+
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+### React Django Lead Manager
+
+Commmands:
+
+
+pip3 install pipenv
+
+pipenv shell
+
+pipenv install django djangorestframework django-rest-knox
+
+django-admin startproject leadmanager .
+
+python manage.py startapp leads
+
+python manage.py makemigrations leads
+
+python manage.py migrate
+
+
